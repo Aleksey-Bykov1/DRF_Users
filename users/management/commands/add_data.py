aa8441299b8e7e61c3b36c88bced662789997e60
Alexey <max_89@inbox.ru>
2022-02-09 11:44:17 +0300

    add_commands_1

--- users/management/commands/add_data.py
+++ users/management/commands/add_data.py
@@ -3,15 +3,15 @@ from users.models import CustomUser
 
 
 class Command(BaseCommand):
-    help = 'Create Superuser and some test users'
+    help = 'Add users'
 
     def add_arguments(self, parser):
-        parser.add_argument('count', type=int)
+        parser.add_argument('num', type=int)
 
     def handle(self, *args, **options):
         CustomUser.objects.all().delete()
-        user_count = options['count']
+        num = options['num']
         CustomUser.objects.create_superuser('alex', '1@1.ru', 'test1234test')
-        for i in range(user_count):
+        for i in range(num):
             CustomUser.objects.create_user(f'user_{i}', f'user_{i}', 'test12345678')
-        print('done')
+        print('Users added')
